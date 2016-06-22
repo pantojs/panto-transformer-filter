aa3b273516a889fc6c3a48c4722a73b418e7a913
yinyong <yanni4night@gmail.com>
2016-06-22 11:21:17 +0800

    ready

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # panto-transformer-fitler
-Filter transformer for panto
+[![NPM version][npm-image]][npm-url] [![Downloads][downloads-image]][npm-url] [![Dependency status][david-dm-image]][david-dm-url] [![Dev Dependency status][david-dm-dev-image]][david-dm-dev-url]
+
+Filter transformer for panto.
+
+```
+panto.loadTransformer('filter');
+
+panto.pick('**/*').(panto.filter({
+    pattern: [/.js$/, filename => {
+        return filename[0] === 'a';
+    }, 'src/**/*']
+})).(panto.read()).end();
+```
+
+## options
+ - pattern: array|regexp|string|function
+
+[npm-url]: https://npmjs.org/package/panto-transformer-filter
+[downloads-image]: http://img.shields.io/npm/dm/panto-transformer-filter.svg
+[npm-image]: http://img.shields.io/npm/v/panto-transformer-filter.svg
+[david-dm-url]:https://david-dm.org/pantojs/panto-transformer-filter
+[david-dm-image]:https://david-dm.org/pantojs/panto-transformer-filter.svg
+[david-dm-dev-url]:https://david-dm.org/pantojs/panto-transformer-filter#info=devDependencies
+[david-dm-dev-image]:https://david-dm.org/pantojs/panto-transformer-filter/dev-status.svg
---
+++ index.js
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2016 panto.xyz
+ * index.js
+ *
+ * changelog
+ * 2016-06-22[09:49:10]:revised
+ *
+ * @author yanni4night@gmail.com
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+'use strict';
+
+const Transformer = require('panto-transformer');
+const minimatch = require('minimatch');
+
+class FilterTransformer extends Transformer {
+    _transform(file) {
+        const {
+            pattern
+        } = this.options;
+
+        const match = pattern => {
+            if (panto.util.isString(pattern) && minimatch(file.filename, pattern)) {
+                return true;
+            } else if (panto.util.isRegExp(pattern) && pattern.test(file.filename)) {
+                return true;
+            } else if (panto.util.isFunction(pattern) && pattern(file.filename)) {
+                return true;
+            }
+
+            return false;
+        };
+
+        let matched = false;
+
+        if (Array.isArray(pattern)) {
+            matched = pattern.some(match);
+        } else {
+            matched = match(pattern);
+        }
+
+        return Promise.resolve(matched ? file : []);
+    }
+}
+
+module.exports = FilterTransformer;
